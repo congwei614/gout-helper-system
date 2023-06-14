d191b0450ed8e48dcf0b212b10af3b3bfd8f70bf
Mr.cong <2534069177@qq.com>
2023-06-14 22:10:42 +0800

    chore: :hammer: 版本控制
    版本控制

---
+++ .versionrc.cjs
@@ -0,0 +1,21 @@
+/*
+ * @Author: Mr.Cong Wei
+ * @Date: 2023-06-14 21:50:55
+ * @LastEditTime: 2023-06-14 21:51:15
+ */
+module.exports={
+    "issueUrlFormat": "https://xxx.com/{{id}}",
+    "types": [
+      { "type": "feat", "section": "需求" },
+      { "type": "fix", "section": "Bug 修复" },
+      { "type": "perf", "section": "优化" },
+      { "type": "docs", "hidden": true },
+      { "type": "style", "hidden": true },
+      { "type": "refactor", "hidden": true} ,
+      { "type": "test", "hidden": true },
+      { "type": "build", "hidden": true },
+      { "type": "ci", "hidden": true },
+      { "type": "chore", "hidden": true },
+      { "type": "revert", "hidden": true }
+    ]
+  }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+<!--
+ * @Author: Mr.Cong Wei
+ * @Date: 2023-06-14 20:25:45
+ * @LastEditTime: 2023-06-14 21:58:58
+-->
+
 # gout-helper-system
+
 gout-helper-system
+
+## 版本控制
